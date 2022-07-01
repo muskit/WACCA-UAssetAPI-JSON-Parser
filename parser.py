c6721ae61fbd421bdd87ae3dfb5bf9e9a8301133
msk <15199219+muskit@users.noreply.github.com>
2022-06-30 17:02:35 -0700

    put the parsing into a function

--- parser.py
+++ parser.py
@@ -3,8 +3,7 @@ from encodings import utf_8
 import json
 from re import M
 
-Song = namedtuple("Song", "id category name artist level audio")
-songs = []
+Song = namedtuple("Song", "id category name artist tempo level level_designer audio")
 
 CATEGORY = {
     '0':'アニメ／ＰＯＰ', #Anime/Pop
@@ -16,6 +15,7 @@ CATEGORY = {
     '6':'TANO*C' #HARDCORE TANO*C
 }
 
+## -- JOG AND OUTLIER DATA GOOD FOR "LILY R" --
 # pair id to arbitrary audio index without altering idx
 SONG_AUDIO_OUTLIERS = {
     'S01-008': 180, #360
@@ -36,10 +36,10 @@ SONG_AUDIO_OUTLIERS = {
     'S01-267': '???',
     'S02-001': '???',
     'S02-021': [209, 210], #418, 420
-    'S02-036': 147, #294
+    'S02-036': [147, 221], #294, 442
 }
 
-# where to re-seek the iterator
+# pair id to arbitrary audio index and reseek idx
 SONG_AUDIO_JOG = {
     'S01-113': 119, #238
     'S01-117': 101, #202
@@ -49,52 +49,93 @@ SONG_AUDIO_JOG = {
     'S01-268': 168, #336
     'S02-003': 193, #386
     'S02-023': 211, #422
+    'S02-037': 222, #444
 }
 
-## LAST KNOWN GOOD: S02-024
+END_OF_GOOD_RANGE = 'S02-037'
 
-f = open('data.json', 'r', encoding='utf_8')
-j = json.loads(f.read())
-data = j['Exports'][0]['Table']['Data']
+# Build music database as ID-keyed dictionary.
+# 
+# jsonPath: path to JSON file exported from UAssetGUI
+# (Content/Table/MusicParameterTable.uasset)
+def get_music_dict(jsonPath: str) -> dict:
+    songs = dict()
 
-for elem in data: # songs
-    id = ''
-    category = ''
-    name = ''
-    artist = ''
-    level = [-1, -1, -1, -1]
-    img = ''
+    f = open(jsonPath, 'r', encoding='utf_8')
+    j = json.loads(f.read())
+    data = j['Exports'][0]['Table']['Data']
 
-    for key in elem['Value']: # properties of song
-        if key['Name'] == 'AssetDirectory':
-            id = key['Value']
-        if key['Name'] == 'ScoreGenre':
-            category = key['Value']
-        if key['Name'] == 'MusicMessage':
-            name = key['Value']
-        if key['Name'] == 'ArtistMessage':
-            artist = key['Value']
-        if key['Name'] == 'DifficultyNormalLv':
-            level[0] = key['Value']
-        if key['Name'] == 'DifficultyHardLv':
-            level[1] = key['Value']
-        if key['Name'] == 'DifficultyExpertLv':
-            level[2] = key['Value']
-        if key['Name'] == 'DifficultyInfernoLv' and key['Value'] != '+0':
-            level[3] = key['Value']
+    for elem in data: # songs
+        id = ''
+        category = ''
+        name = ''
+        artist = ''
+        tempo = -1
+        level = [-1, -1, -1, -1]
+        level_designer = ['', '', '', '']
+        img = ''
+
+        for key in elem['Value']: # properties of song
+            if key['Name'] == 'AssetDirectory':
+                id = key['Value']
+            #SongInfo
+            if key['Name'] == 'ScoreGenre':
+                category = key['Value']
+            if key['Name'] == 'MusicMessage':
+                name = key['Value']
+            if key['Name'] == 'ArtistMessage':
+                artist = key['Value']
+            if key['Name'] == 'Bpm':
+                tempo = key['Value']
+            #ChartInfo Levels
+            if key['Name'] == 'DifficultyNormalLv':
+                level[0] = key['Value']
+            if key['Name'] == 'DifficultyHardLv':
+                level[1] = key['Value']
+            if key['Name'] == 'DifficultyExtremeLv':
+                level[2] = key['Value']
+            if key['Name'] == 'DifficultyInfernoLv' and key['Value'] != '+0':
+                level[3] = key['Value']
+            #ChartInfo Designers
+            if key['Name'] == 'NotesDesignerNormal':
+                level_designer[0] = key['Value']
+            if key['Name'] == 'NotesDesignerHard':
+                level_designer[1] = key['Value']
+            if key['Name'] == 'NotesDesignerExpert':
+                level_designer[2] = key['Value']
+            if key['Name'] == 'NotesDesignerInferno':
+                level_designer[3] = key['Value']
+        
+        songs[id] = Song(id, category, name, artist, tempo, level, level_designer, -1)
+
+    # sort songs by ID
+    idx = 0
+    songs = dict(sorted(songs.items()))
+
+    # assign audio indices
+    for song in songs.items():
+        if song[1].id in SONG_AUDIO_OUTLIERS:
+            songs[song[0]] = song[1]._replace(audio = SONG_AUDIO_OUTLIERS[song[1].id])
+        else:
+            if song[1].id in SONG_AUDIO_JOG:
+                idx = SONG_AUDIO_JOG[song[1].id]
+            songs[song[0]] = song[1]._replace(audio = idx)
+            idx += 1
     
-    songs.append(Song(id, category, name, artist, level, -1))
+    return songs
 
-# sort songs by ID and assign audio indices
-idx = 0
-songs.sort(key=lambda song: song.id)
-for song in songs:
-    if song.id in SONG_AUDIO_OUTLIERS:
-        song = song._replace(audio = SONG_AUDIO_OUTLIERS[song.id])
-    else:
-        if song.id in SONG_AUDIO_JOG:
-            idx = SONG_AUDIO_JOG[song.id]
-        song = song._replace(audio = idx)
-        idx += 1
-
-    print(f"{song.id}: {song.name} - {song.audio} ({2*song.audio})")
+if __name__ == "__main__":
+    songs = get_music_dict('data.json')
+    # print out songs
+    for song in songs.items():
+        audio_str = ''
+        if type(song[1].audio) == list:
+            audio_doubledoffset = []
+            for bpm in song[1].audio:
+                audio_doubledoffset.append(2*bpm)
+            audio_str = f'{song[1].audio} ({audio_doubledoffset})'
+        else:
+            audio_str = f'{song[1].audio} ({2*song[1].audio})'
+        print(f"{song[1].id}: {song[1].name} - {audio_str}")
+        if song[1].id == END_OF_GOOD_RANGE:
+            print("------ END KNOWN GOOD AUDIO ------\n")
